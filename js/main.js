38fc9bcfd73c2cd529eb1a41c51d5ecd20557ba2
Anatoly230 <https://github.com/Anatoly230>
2022-10-10 02:20:01 +0300

    первые строки кода

---
+++ js/main.js
@@ -0,0 +1,34 @@
+console.log("hi")
+
+function getValueOfArguments(from, to) {
+    if (from < 0) {
+        from = Math.abs(from);
+    }
+    if (from > to) {
+        [from, to] = [to, from]
+    }
+
+    return {
+        from: from,
+        to: to
+    }
+}
+
+function getRandomNum(from = 1000, to = 0) {
+    let range = getValueOfArguments(from, to);
+    return Math.floor(Math.random() * (range.to - range.from + 1)) + range.from;
+}
+
+function getRandomFloat(from = 1000, to = 0, countNum = 3) {
+    let range = getValueOfArguments(from, to);
+    return Number((Math.random() * (range.to - range.from + 1) + range.from).toFixed(countNum));
+}
+
+getRandomNum(3, 0);
+
+function defineStringLength(str, charCount) {
+    if (str.length > charCount) {
+        return false;
+    }
+    return true;
+}
