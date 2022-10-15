e6c2ce11e47eb7ec2a82db258ee87ce5e989d170
Anatoly230 <https://github.com/Anatoly230>
2022-10-15 20:42:44 +0300

    Добавил все входные данные, перехожу писать функцию

--- js/main.js
+++ js/main.js
@@ -1,66 +1,71 @@
 console.log("hi")
 
 function getValueOfArguments(from, to) {
-    if (typeof from !== "number") {
-        return false;
-    }
-    if (from < 0) {
-        from = Math.abs(from);
-    }
-    if (from > to) {
-        [from, to] = [to, from];
-    }
+  if (typeof from !== "number") {
+    return false;
+  }
+  if (from < 0) {
+    from = Math.abs(from);
+  }
+  if (from > to) {
+    [from, to] = [to, from];
+  }
 
-    return {
-        from: from,
-        to: to
-    }
+  return {
+    from: from,
+    to: to
+  }
 }
 
 function getRandomNum(from = 1000, to = 0) {
-    try {
-        let range = getValueOfArguments(from, to);
-        return Math.floor(Math.random() * (range.to - range.from + 1)) + range.from;
-    } catch (err) {
-        console.log(err)
-        return false;
-    }
+  try {
+    let range = getValueOfArguments(from, to);
+    from = range.from,
+      to = range.to;
+    range = null;
+    return Math.floor(Math.random() * (to - from + 1)) + from;
+  } catch (err) {
+    console.log(err)
+    return false;
+  }
 
 }
 
 function getRandomFloat(from = 1000, to = 0, countNum = 3) {
-    try {
-        let range = getValueOfArguments(from, to);
-        return Number((Math.random() * (range.to - range.from + 1) + range.from).toFixed(countNum));
-    } catch (err) {
-        console.log(err.stack);
-        return false;
-    }
+  try {
+    let range = getValueOfArguments(from, to);
+    from = range.from,
+      to = range.to;
+    range = null;
+    return Number((Math.random() * (to - from + 1) + from).toFixed(countNum));
+  } catch (err) {
+    console.log(err.stack);
+    return false;
+  }
 }
 
 getRandomNum(3, 0);
 
 function defineStringLength(str, charCount) {
-    if (str.length > charCount) {
-        return false;
-    }
-    return true;
+  if (str.length > charCount) {
+    return false;
+  }
+  return true;
 }
 
-let arr = ["привет",  "как", "дела?"]
+let arr = ["привет", "как", "дела?"]
 console.log(arr["2-2"])
 
-function makeCount(){
-    let count = 0,
+function makeCount() {
+  let count = 0,
     str = "Привет, я лексическое окружение функции, которые возвращает эта, изменений "
-    return function (){
-        console.log(count++, str + count)
-    }
+  return function () {
+    console.log(count++, str + count)
+  }
 }
 
-let counter = makeCount();
-// makeCount()();
-// makeCount()();
-counter();
-counter();
-console.log(Number.hasOwnProperty(parseInt))
+// let counter = makeCount();
+
+// counter();
+// counter();
+
