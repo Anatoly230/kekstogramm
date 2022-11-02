29fbb59ae143fd1606b7be18cf20db62640176b9
Anatoly230 <maximos225@mail.ru>
2022-11-02 04:56:34 +0300

    5-й раздел завершил

--- js/main.js
+++ js/main.js
@@ -2,3 +2,15 @@
 import {getPhotos} from "./data.js";
 
 getPhotos();
+
+
+
+const html = document.documentElement;
+for (let i = 0; i < html.children.length; i++) {
+    const child = html.children[i];
+    console.log(child.tagName.toLowerCase());
+    for (let j = 0; j < child.children.length; j++) {
+        const innerChild = child.children[j];
+        console.log('|---' + innerChild.tagName.toLowerCase());
+    }
+}
